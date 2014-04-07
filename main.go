8bee99648f441904fbe2d0730f445c00c496206a
Julius Foitzik <info@accountr.eu>
2014-04-08 00:34:59 +0200

    initial commit initial commit 22

---
+++ main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/gorilla/securecookie"
+	"github.com/gorilla/sessions"
+	"net/http"
+	"time"
+)
+
+const SessionSecret = "sdngosngpasp4oa7ta8473280gtgadfgh"
+const SessionName = "gosess"
+const HostName = "localhost"
+
+var store = sessions.NewCookieStore([]byte(SessionSecret))
+var hashKey = []byte{34, 35, 35, 57, 68, 4, 35, 36, 7, 8, 35, 23, 35, 86, 35, 23}
+var blockKey = []byte{34, 35, 35, 57, 68, 4, 35, 36, 7, 8, 35, 23, 35, 86, 35, 23}
+
+func initSessionOptions() {
+	store.Options = &sessions.Options{
+		Domain:   HostName,
+		Path:     "/",
+		MaxAge:   3600 * 8, // 8 hours
+		HttpOnly: true,
+	}
+}
+
+func IsAuth(r *http.Request) bool {
+	session, _ := store.Get(r, SessionName)
+	var s = securecookie.New(hashKey, blockKey)
+	var secureCookie string
+	sessionValue, ok := session.Values["security"].(string)
+	if ok {
+		s.Decode("security", sessionValue, &secureCookie)
+		fmt.Printf("SecurityCookie: %s", secureCookie)
+	} else {
+		return false
+		fmt.Println("SecurityCookie: %s", "ERROR")
+	}
+}
+
+func DefaultHandler(w http.ResponseWriter, r *http.Request) {
+	// Get a session. We're ignoring the error resulted from decoding an
+	// existing session: Get() always returns a session, even if empty.
+	session, _ := store.Get(r, SessionName)
+
+	var s = securecookie.New(hashKey, blockKey)
+
+	// Set some session values.
+	const layout = "Jan 2, 2006 at 3:04pm (MST)"
+	secureCookie, err := s.Encode("security", time.Now().Format(layout))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	session.Values["security"] = secureCookie
+	// Save it.
+	err = session.Save(r, w)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	// Get a session. We're ignoring the error resulted from decoding an
+	// existing session: Get() always returns a session, even if empty.
+	session, _ := store.Get(r, SessionName)
+
+	var s = securecookie.New(hashKey, blockKey)
+
+	// Set some session values.
+	var secureCookie string
+	sessionValue, ok := session.Values["security"].(string)
+	if ok {
+		s.Decode("security", sessionValue, &secureCookie)
+		fmt.Fprintf(w, "SecurityCookie: %s", secureCookie)
+	} else {
+		fmt.Fprintf(w, "SecurityCookie: %s", "ERROR")
+	}
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", DefaultHandler)
+	r.HandleFunc("/login", LoginHandler)
+	http.Handle("/", r)
+	http.ListenAndServe(":8081", nil)
+}
---
+++ structs.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"time"
+)
+
+type Game struct {
+	Name  string
+	Genre string
+}
+
+type Lobby struct {
+	Title              string
+	Created            time.Time
+	Updated            time.Time
+	Access             string
+	Owner              *User
+	State              string
+	Game               *Game
+	SkillLevel         int
+	EstimatedPlayTime  int
+	EstimatedStartTime time.Time
+}
+
+type User struct {
+	Name      string
+	Email     string
+	Password  string
+	Hash      string
+	LastLogin time.Time
+	Created   time.Time
+	Lobby     *Lobby
+}
+
+func NewGame(name string, genre string) *Game {
+	return &Game{
+		Name:  name,
+		Genre: genre,
+	}
+}
+
+func NewLobby(title string, access string, owner *User, state string, game *Game, skillLevel int, estimatedPlayTime int, estimatedStartTime time.Time) *Lobby {
+	return &Lobby{
+		Title:              title,
+		Created:            time.Now(),
+		Owner:              owner,
+		State:              state,
+		Game:               game,
+		SkillLevel:         skillLevel,
+		EstimatedPlayTime:  estimatedPlayTime,
+		EstimatedStartTime: estimatedStartTime,
+	}
+}
+
+func NewUser(name string, email string, password string, hash string, lobby *Lobby) *User {
+	return &User{
+		Name:     name,
+		Email:    email,
+		Password: password,
+		Hash:     hash,
+		Lobby:    lobby,
+	}
+}
